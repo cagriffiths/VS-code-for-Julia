a55981bfe3f42924d0146211247f279dfbebc0cf
Eva Delmas <eva.delmas@gmail.com>
2020-11-01 15:51:27 -0500

    :white_check_mark: file 1

--- Using Julia in VS code #1.jl
+++ Using Julia in VS code #1.jl
@@ -21,7 +21,7 @@
 # (5) Then click on explorer symbol (top symbol on the activity bar) and click Open Folder
 # (6) Navigate to our folder of choice and click open - this will then form the location of your directory (same as an RProject). Your directory will appear as a vertical pane on the left hand side of the screen. 
 # (4) You can then create a new file (a script) using (cmd-N, File>New File or left click>New File in the directory) and save it with .jl extension (tells VS code you want to use the Julia language)
-# (5) Open a new REPL (stands for - read, execute, print and loop) by typing Start REPL in the command palette (accessed using F1, cmd-shift-P or View>Command Palette). The REPL is the console/terminal that you run Julia code in. 
+# (5) Open a new REPL (stands for - read, execute, print and loop) by either typing Alt-J Alt-O or by typing Start REPL in the command palette (accessed using F1, cmd-shift-P or View>Command Palette). The REPL is the console/terminal that you run Julia code in. 
 # (6) Type 'Hello world' in your new script and run (ctrl-enter to run line by line or shift-enter to run a block of code):
 println("Hello world")
 
@@ -30,9 +30,14 @@ println("Hello world")
 import Pkg
 Pkg.activate(".") # Activites the directory and points to the Manifest.toml and Project.toml files that are automatically created. 
 Pkg.instantiate() # Instantiate ensures that right version of all packages are being installed - key when you want to work from a different computer and need to install packages, or for other users of your code.
+# There are two ways to check taht you are actually working into the right environment: 
+# - check the 'Julia env:...' on the bottom left of your screen, it should match your folder name
+# - enter the package manager by typing ] in the Julia REPL, you should see '(your-folder-name) pkg>' instead of 'julia>'. Exit it using backspace.
+
 # (2) To install a new package you use:
 Pkg.add("CSV")
 # or you can type the following directly in to the REPL: ] add CSV (if you use this approach, make sure you backspace out of the project directory). You can also remove a package this way using ] rm CSV
+
 # (3) You then use 'using' to tell Julia that you want to use a given package:
 using CSV
 # Other useful packages include:
@@ -47,8 +52,6 @@ using Plots, DataFrames, Distributions, Random, EcologicalNetworks, BioEnergetic
 # (4) You can then check the packages and versions that are currently active in your directory using:
 Pkg.status() # useful when using the BioEnergeticFoodWebs package as Eva has a ton of versions...
 
-## Eva - Should we put something hear about linking to GitHub? Third symbol down in the activity bar... 
-
 ## (3) Basic commands 
 # (1) Set a random seed:
 Random.seed!(33)
@@ -57,7 +60,8 @@ aps = "Animal and Plant Sciences" # must use "" and not ''
 # (3) Allocate an integer:
 number = 5
 # Note that you can easily insert variables into strings using $ and concatenate strings using * : 
-println("This is a number: $number" * " - type: Integer")
+t = typeof(number) #you can use typeof to identify the type of an object
+println("This is a number: $number" * " - type: $t")
 # (4) Allocate a floating point number:
 pi_sum = 3.1415
 # note: pi can also be called using pi or π (type \pi and tab to transform into the unicode symbol)
@@ -67,16 +71,17 @@ pi_sum3 = π
 λ = 4
 # and you can attribute multiple variables at the same time: 
 αi, βi, γi = 1.3, 2.1, exp(39)
+αi
 # Note - Julia is like R and Python, it can infer the type of object (Integer, Float) on the left hand side of the equals sign, you don't have to justify it like you do in C. 
+# Reading more about Julia's type system is actually a good idea to understand more about its behaviour: 
 # However, you can if needed e.g.
 pi_sum1 = Float64(3.141592)
 # (5) You can then check the object type using:
-typeof(number)
 typeof(pi_sum), typeof(pi_sum2) #note here that by using the preallocated variable pi, we are actually using an object of type Irrational (a specific type of float)
 # (6) You can print an object (useful when running simulations) using:
 print(aps) 
 print(aps) # prints in the same line or
-println(aps) # prints on the next line
+println(aps) # prints on the next line (useful for printing in loops, etc)
 # (7) and convert between object types using:
 a = 2 
 b = convert(Float64, a) 
